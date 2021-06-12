327edcdddaba83e29b8f9269de202f19002e2963
nico <tramber30@gmail.com>
2021-06-12 14:07:04 +0200

    Remove share with skype contextual menu

---
+++ automatic_skype_remover.ps1
@@ -0,0 +1,6 @@
+# Automatically remove "share with skype" from contextual menu
+
+$Skype_reg_entry = Get-ChildItem -path 'HKLM:\SOFTWARE\Classes\PackagedCom\Package' | Where-Object {$_.name -match  "Microsoft.SkypeApp_*"}
+$Full_path = "registry::" + $skype_reg_entry.Name + "\Class\{776DBC8D-7347-478C-8D71-791E12EF49D8}"
+Write-Host "remove key: [$Full_path]"
+Remove-Item -Path $Full_path
